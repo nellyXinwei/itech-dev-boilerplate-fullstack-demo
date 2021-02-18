14bb1d60a7ab61637a7e5fa9ef58bd3f2803994f
nellyXinwei <nelsonalbajr@yahoo.com>
2021-02-18 20:37:11 +0800

    finished reading single articles

--- frontend/src/pages/Article.vue
+++ frontend/src/pages/Article.vue
@@ -1,10 +1,28 @@
 <template>
-  <div>Article Page</div>
+  <div>
+    <h1>{{ article.name }} by {{ article.author.name }}</h1>
+    <p>{{ article.content }}</p>
+  </div>
 </template>
 
 <script>
+import axios from "axios";
 export default {
   name: "Article",
+
+  data() {
+    return {
+      article: [],
+    };
+  },
+
+  mounted() {
+    const id = this.$route.params.id;
+    axios.get(`http://localhost:1337/articles/${id}`).then((response) => {
+      this.article = response.data;
+      // console.log(this.articles);
+    });
+  },
 };
 </script>
 
